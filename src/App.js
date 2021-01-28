1c0ae9e58a13ddc17697e40a47582fd0c1c50e12
Carson <azzlamusic@hotmail.com>
2021-01-27 19:35:56 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ToDoList from './components/ToDoList';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <ToDoList/>
+    )
+  }
+}
+
+
+export default App;
---
+++ src/components/ToDoList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ListView from './ListView';
+
+class ToDoList extends Component {
+  render() {
+    return (
+      <ListView/>
+      //<AddToList/>
+      //<ClearList/>
+    )
+  }
+}
+
+
+export default ToDoList;
